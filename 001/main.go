88634b4d8b792b1edb0f231949e0d35fbdcb927c
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-20 15:33:17 +0430

    Project 001 solved.

---
+++ 001/main.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+
+func main() {
+
+	sum := 0
+	for i := 0; i < 1000 ; i++ {
+		if i % 3 == 0  || i % 5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println("Sum of all mutiple of 3 or 5 below 10 is: ", sum)
+}
